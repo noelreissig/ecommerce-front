75bdd76c7730021beb9d4a90320db0543f57b297
AntonellaGervasini <gervasini.antonella@gmail.com>
2021-09-01 12:29:14 -0300

    Confirmaciones de datos para compra

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Cart from "./components/pages/Cart/Cart";
 import Buy from "./components/pages/Buy/Buy";
 import MyAccount from "./components/pages/MyAccount/MyAccount";
 import PrivateRoute from "./components/PrivateRoute";
+import ConfirmCard from "./components/pages/BuyCompleted/ConfirmCard";
 
 function App() {
   return (
@@ -21,8 +22,9 @@ function App() {
           <Route path="/contacto" component={Contact} />
           <Route path="/sobre-nosotros" component={AboutUs} />
           <Route path="/carrito" component={Cart} />
-          <PrivateRoute path="/comprar" component={Buy} />
           <PrivateRoute path="/mis-datos" component={MyAccount} />
+          <PrivateRoute path="/comprar" component={Buy} />
+          <PrivateRoute path="/confirmar-tarjeta" component={ConfirmCard} />
           <Route path="/gracias-por-su-compra" component={BuyCompleted} />
           <Route path="/producto/:slug" component={OneProduct} />
           <Route path="/:categories" component={Category} />
