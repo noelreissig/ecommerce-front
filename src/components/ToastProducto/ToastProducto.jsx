6bb22579ea4a1a838f6b9936d232c4f46cc82b0a
noelreissig <NREISSIG@GMAIL.COM>
2021-09-07 23:22:40 -0700

    zindex toast product

--- src/components/ToastProducto/ToastProducto.jsx
+++ src/components/ToastProducto/ToastProducto.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Toast, ToastContainer } from "react-bootstrap";
 const ToastProducto = ({ show, setShow }) => {
+  const style = {
+    marginTop: "65px",
+    zIndex: "5",
+  };
+
   return (
     <div className="">
       <ToastContainer
         position="top-center"
-        style={{ marginTop: "65px" }}
-        className="position-fixed"
+        style={style}
+        className="position-fixed overlay"
       >
         <Toast
           onClose={() => setShow(false)}
